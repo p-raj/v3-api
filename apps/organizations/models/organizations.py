b096ca5ba8cd97657ed0fe31f64b8ee7672a1b1a
Ankit Popli <ankit.popli@veris.in>
2016-11-19 19:55:45 +0530

    feat: auto add admin to the members

--- apps/organizations/models/organizations.py
+++ apps/organizations/models/organizations.py
@@ -9,6 +9,35 @@ from django.contrib import admin
 from django.db import models
 
 
+class OrganizationQuerySet(models.QuerySet):
+    """
+    A custom queryset? Yeah, let's make an assumption that the user
+    creating the organization needs will always be a member of the organization with
+    some defined ``role`` or ``permission``.
+    """
+
+    def create(self, **kwargs):
+        # create not called ? strange ? - no
+        # admin panel calls Model.save directly
+        # we can do this there on a deeper level,
+        # but actually this is the right place as
+        # we'll be calling ``objects.create``
+
+        # no, no, no, we don't want to change the default functionality at all :)
+        obj = super(OrganizationQuerySet, self).create(**kwargs)
+
+        # let's just added the user who created the organization
+        # as the member of the organization as well
+        # lazy import to prevent cyclic imports
+        from .members import Member
+        Member.objects.create(**{
+            'user_id': obj.user_id,
+            'organization': obj
+        })
+
+        return obj
+
+
 class Organization(models.Model):
     """
     Organization model directly extends `models.Model`, yeah only for now.
@@ -28,6 +57,11 @@ class Organization(models.Model):
 
     # lets add other attributes as and when required and necessary
     # lazy is the new awesome :P
+
+    # lets change the default manager
+    # why? we need to add the organization creator as a member by default
+    # will ease things out for access management, at least it seems like it for now
+    objects = OrganizationQuerySet.as_manager()
 
     def __str__(self):
         return self.name
