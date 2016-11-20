ca38695b4754c6a4214eec003b4830571008fe81
Ankit Popli <ankit.popli@veris.in>
2016-11-20 18:33:04 +0530

    feat: auto create a shadow user whwnever requested

--- apps/contrib/views/users.py
+++ apps/contrib/views/users.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from rest_framework import viewsets
 
 from ..serializers.users import User, UserSerializer
@@ -8,3 +9,13 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     lookup_field = 'username'
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            self.get_object()
+        except Http404:
+            serializer = self.get_serializer(data=kwargs)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+
+        return super(UserViewSet, self).retrieve(request, *args, **kwargs)
