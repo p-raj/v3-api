db5869a8f45cd1a0e3752b8f2787586b6ef273fb
Ankit Popli <ankit.popli@veris.in>
2016-11-23 15:19:00 +0530

    feat: introduce Terminal as a resource

---
+++ apps/terminals/models/terminal.py
@@ -0,0 +1,51 @@
+"""
+Terminal is a Resource.
+
+### Previously:
+ - Terminal was a part of Organization, making it tightly coupled with Organization.
+ - A User can not create a terminal unless he creates Organization & Venues.
+ - A Venue `Address` dictates where to **expect** the terminal. A venue may have address
+    but that doesn't guarantee the locations of terminals.
+
+User App, is it a terminal ? to which organization it is bound ?
+Veris ? Why do we need one ?
+Do we really need one ?
+
+Let's follow a slightly different path and try to see our
+beloved terminal from a new perspective.
+
+ - Terminal is a standalone Resource.
+ - It can be created by any `User`. User may or may not have any organizations.
+ - If the User has an `Organization`, he can bind the membership definition using `Rules/Scopes`.
+ - Terminal uses Location API across different platforms to update its location.
+
+Let's encounter some cases / doubts:
+
+ - Nested Venues ? Why we made nested venues ? or even venues ?.
+    Venues: [group of T] [Hierarchy of T => Lift is accessible to all, but not the Data Center]
+        - A group of terminals having same *properties/settings !
+        - Give access to child venues and you have access to parent venues.
+            Conclusion: for easier access management for `users` & `groups`.
+
+    React Principles to rescue. Let's try Composition instead of Inheritance.
+        - A terminal has some resources `Properties/Settings`.
+        - Create a Property and assign it to multiple Terminals.
+        - Define access and use it for multiple terminals.
+
+    Give N members of X Org access to T Terminals. Done :+1:
+    * Only condition: Admin must have access to all :arrow_up: resources.
+
+ - How will Organizations manage Terminals ?
+    A `User` having both Organizations & Terminals can define Rules like:
+        - Org X & Org Y can manage Terminal T. :+1:
+        - Org X can manage T1, T2, T5. Org Y can manage T2 & T3. :+1:
+
+ - Show Nearby Terminals. Done. :+1:
+
+"""
+
+from django.db import models
+
+
+class Terminal(models.Model):
+    pass
