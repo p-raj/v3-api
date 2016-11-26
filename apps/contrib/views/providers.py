37b99671183b80669d2bc1b7ec170191c5540d9e
Ankit Popli <ankit.popli@veris.in>
2016-11-26 15:09:39 +0530

    feat: add temporary redirection to open deeplink enabled client

--- apps/contrib/views/providers.py
+++ apps/contrib/views/providers.py
@@ -3,6 +3,8 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 
+from django.http.response import HttpResponse
+
 from rest_auth.registration.views import SocialLoginView
 
 
@@ -10,6 +12,15 @@ class GithubAPIView(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
     client_class = OAuth2Client
     callback_url = 'http://192.168.1.96:8000/accounts/github/login/callback/'
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        response = HttpResponse('', status=302)
+        response['Location'] = 'in.veris.terminal://veris?code={code}&state={state}' \
+            .format(**request.query_params.dict())
+        return response
 
 
 class GoogleAPIView(SocialLoginView):
