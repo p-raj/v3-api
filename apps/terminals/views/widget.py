851d2f19c3e57a7996a66369e4d5caa7b7f29200
Ankit Popli <ankit.popli@veris.in>
2016-12-01 16:54:34 +0530

    feat: serve authentication reqs based on different configs

--- apps/terminals/views/widget.py
+++ apps/terminals/views/widget.py
@@ -1,14 +1,17 @@
 import importlib
+import json
 
 from allauth.socialaccount.models import SocialApp
 from apps.contrib.serializers.social_app import SocialAppSerializer
 
 from django.contrib.sites.models import Site
 
+from apps.terminals.models.widget_container import WidgetContainer
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED, \
+    HTTP_405_METHOD_NOT_ALLOWED
 
 from ..serializers.widget import Widget, WidgetAdminSerializer
 
@@ -31,8 +34,25 @@ class WidgetViewSet(viewsets.ModelViewSet):
     queryset = Widget.objects.all()
     serializer_class = WidgetAdminSerializer
 
+    def _get_config(self, request, pk=None):
+        if hasattr(self, '_widget_config'):
+            return self._widget_config
+
+        data = request.query_params.dict()
+        # we'll get this from request somehow
+        # asking this from params for now :(
+        # TODO
+        container = WidgetContainer.objects.get(pk=data.get('container'))
+        self._widget_config = json.loads(container.config)
+        return self._widget_config
+
     @detail_route(methods=['GET', 'POST'])
     def authenticate(self, request, pk=None):
+        config = self._get_config(request, pk)
+
+        if not bool(config.get('allow_form_login')):
+            return Response(status=HTTP_405_METHOD_NOT_ALLOWED)
+
         data = request.query_params.dict()
         from django.contrib.auth import authenticate
         user = authenticate(**data)
@@ -40,7 +60,7 @@ class WidgetViewSet(viewsets.ModelViewSet):
         if not user:
             return Response(status=HTTP_401_UNAUTHORIZED)
 
-        if data.get('mfa') == 'true':
+        if bool(config.get('is_mfa_enabled')):
             return Response(status=HTTP_200_OK, headers={
                 'X-HTTP-MFA': 'totp',
                 'X-MFA-TOKEN': 'mfa-token-of-user'
@@ -54,6 +74,10 @@ class WidgetViewSet(viewsets.ModelViewSet):
 
     @detail_route(methods=['GET', 'POST'])
     def mfa(self, request, pk=None):
+        config = self._get_config(request, pk)
+        if not bool(config.get('is_mfa_enabled')):
+            return Response(status=HTTP_405_METHOD_NOT_ALLOWED)
+
         data = request.query_params.dict()
         if data.get('otp') != '112233':
             return Response(status=HTTP_401_UNAUTHORIZED)
@@ -65,14 +89,12 @@ class WidgetViewSet(viewsets.ModelViewSet):
 
     @detail_route(methods=['GET'])
     def providers(self, request, pk=None):
-        # TODO
-        # filtering social app should
-        # be according to widget config
+        config = self._get_config(request, pk)
         site = Site.objects.get_current(request)
+
         social_apps = SocialApp.objects.filter(
             sites=site,
-            # from the widget config
-            # provider__in=[]
+            provider__in=config.get('providers')
         )
         serializer = SocialAppSerializer(instance=social_apps, many=True,
                                          context=self.get_serializer_context())
