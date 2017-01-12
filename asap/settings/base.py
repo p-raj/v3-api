68676d3d2eede08d57c08c25043a3c26b84943e7
Ankit Popli <ankit.popli@veris.in>
2017-01-12 14:26:21 +0530

    refactor: move apps package in project namespace

--- asap/settings/base.py
+++ asap/settings/base.py
@@ -234,7 +234,7 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
-    'apps.organizations',
+    'asap.apps.organizations',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- asap/urls.py
+++ asap/urls.py
@@ -13,18 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from rest_framework_nested import routers
-
-from apps.libs.django import UserViewSet
-from apps.organizations.views import MemberViewSet, OrganizationViewSet
-
 from django.conf import settings
 from django.conf.urls import url, include
-
 from django.contrib import admin
-
+from rest_framework_nested import routers
 from rest_framework_swagger.views import get_swagger_view
 
+from asap.apps.organizations.views import MemberViewSet
 from .router import Router
 
 schema_view = get_swagger_view(title='Veris Organizations API')
