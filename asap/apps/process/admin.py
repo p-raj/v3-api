2bf9e27a622d2f76e8e0ec1a3693b61a9a8f0e3e
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-01-17 13:06:46 +0530

    :hammer: add : Process Models Process modcels and Admin panel functions added

--- asap/apps/process/admin.py
+++ asap/apps/process/admin.py
@@ -1,3 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+# future
+from __future__ import unicode_literals
+
+# django
 from django.contrib import admin
 
-# Register your models here.
+# own app
+from asap.apps.process import models
+
+class ProcessAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'resource_token', )
+    list_display_links = ('name', 'code', )
+
+class ProcessLockerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'token', 'is_publish', )
+    list_display_links = ('name', 'token', )
+
+
+admin.site.register(models.Process, ProcessAdmin)
+admin.site.register(models.ProcessLocker, ProcessLockerAdmin)
