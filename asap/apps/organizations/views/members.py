d893b26698023ca294fb9e58ecf9184e4544fa27
Ankit Popli <ankit.popli@veris.in>
2017-01-17 17:44:12 +0530

    refactor: use DRFNestedMixin for nested urls

--- asap/apps/organizations/views/members.py
+++ asap/apps/organizations/views/members.py
@@ -1,25 +1,17 @@
-from rest_framework import response, status, viewsets
+from rest_framework import viewsets
 
+from asap.core.views import DRFNestedViewMixin
 from asap.router import Router
 from ..serializers.members import Member, MemberSerializer
 
 
-class MemberViewSet(viewsets.ModelViewSet):
+class MemberViewSet(DRFNestedViewMixin, viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
-    def list(self, request, *args, **kwargs):
-        if not kwargs.get('organization_pk'):
-            # let's disable the direct access of this url :)
-            # saves a lot of permission checking etc etc
-            # update:
-            # let's assume if the organization pk is missing
-            # the user wants to access all of its memberships
-            self.queryset = self.queryset.filter(user=request.user)
-            return super(MemberViewSet, self).list(request, *args, **kwargs)
-
-        self.queryset = self.queryset.filter(organization=kwargs.get('organization_pk'))
-        return super(MemberViewSet, self).list(request, *args, **kwargs)
+    lookup_parent = [
+        ('organization_pk', 'organization')
+    ]
 
 
 # we have created the nested routes as well
--- asap/apps/vrt/views/runtime.py
+++ asap/apps/vrt/views/runtime.py
@@ -1,30 +1,24 @@
 from asap.apps.vrt.models.runtime import Runtime
 from asap.apps.vrt.serializers.runtime import RuntimeSerializer
+from asap.core.views import DRFNestedViewMixin
 from asap.router import Router
 
 from rest_framework import viewsets
 
 
-class RuntimeViewSet(viewsets.ModelViewSet):
+class RuntimeViewSet(DRFNestedViewMixin, viewsets.ModelViewSet):
     queryset = Runtime.objects.all()
     serializer_class = RuntimeSerializer
 
     lookup_field = 'uuid'
+    lookup_parent = [
+        ('runtime_locker_uuid', 'runtimelocker__uuid')
+    ]
 
     def perform_create(self, serializer):
         # let's assume the we have a middleware that ensures
         # only authenticated & authorized users reach here
         serializer.save(user_id=self.request.user.id)
-
-    def list(self, request, *args, **kwargs):
-        if not kwargs.get('runtime_locker_uuid'):
-            # let's assume if the runtime locker pk is missing
-            # the user wants to access all of hte runtimes created by him
-            self.queryset = self.queryset.filter(user=request.user)
-            return super(RuntimeViewSet, self).list(request, *args, **kwargs)
-
-        self.queryset = self.queryset.filter(runtimelocker__uuid=kwargs.get('runtime_locker_uuid'))
-        return super(RuntimeViewSet, self).list(request, *args, **kwargs)
 
 
 router = Router()
