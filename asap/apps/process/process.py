70f0d73848c73cb6f27a84ab3f8c51f2fcec7243
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-01-18 18:54:24 +0530

    update : Process Logs Logs functionality implemented

--- asap/apps/process/process.py
+++ asap/apps/process/process.py
@@ -12,22 +12,30 @@
 # future
 from __future__ import unicode_literals
 
+# DRF
+from rest_framework.exceptions import ValidationError
+
 # 3rd party
 import requests
 
 # own app
 from asap.apps.process import models, RESOURCE_UPSTREAM_URL
 
+
 class Process(object):
     """A Process class for accessing various process operations and RESTful service.
 
     """
     model = models.Process
-    def __init__(self, code):
+    logging_cls = None
+
+    def __init__(self, code, logging_cls):
         """
 
         :param code: process code
+        :param logging_cls: Logging class instance.
         """
+        self.logging_cls = logging_cls
         self.process_obj = self.model.objects.get(code=code)
 
     def execute_process(self, data={}):
@@ -36,11 +44,20 @@ class Process(object):
         :param data: payload to sent with request
         :return: depends on resource being called by Process
         """
+        resource_token = self.process_obj.resource_token
         url = str(RESOURCE_UPSTREAM_URL).format(
-            resource_token=self.process_obj.resource_token,
+            resource_token=resource_token,
             operation=self.process_obj.operation,
         )
+
+        self.logging_cls.handshake(resource_token, data)  # execution handover initiated
+
         rq = requests.post(url=url,
                            data=data
         )
-        return  rq.json()
+        if rq.status_code == requests.codes.ok:
+            self.logging_cls.handshake_succeed(resource_token, data, rq)  # execution handover status
+            return rq.json()
+
+        self.logging_cls.handshake_failed(resource_token, data, rq.status_code, rq.json())  # execution handover status
+        raise ValidationError(rq.json())
--- asap/apps/process/views.py
+++ asap/apps/process/views.py
@@ -11,13 +11,20 @@
 # future
 from __future__ import unicode_literals
 
+# 3rd party
+import uuid
+
 # rest-framework
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework import permissions
 
+# local
+from asap.apps.logs import logging
+
 # own app
-from  asap.apps.process import models, process
+from asap.apps.process import models, process
+
 
 class ProcessLockerViewSet(viewsets.GenericViewSet):
     """Process Locker Viewset, every process http request handles by this class
@@ -27,6 +34,9 @@ class ProcessLockerViewSet(viewsets.GenericViewSet):
     """
     model = models.ProcessLocker
     permission_classes = (permissions.AllowAny, )
+    actor = 'process'
+    session = uuid.uuid4()
+    logging_cls = None
 
     def get_locker_object(self, token):
         """
@@ -46,7 +56,7 @@ class ProcessLockerViewSet(viewsets.GenericViewSet):
 
         process_response = []
         for process_token in obj.rules:
-            process_cls = process.Process(process_token)
+            process_cls = process.Process(process_token, self.logging_cls)
             process_response.append(process_cls.execute_process(data))
         return process_response
 
@@ -57,8 +67,34 @@ class ProcessLockerViewSet(viewsets.GenericViewSet):
         :param token: process locker token, helps in identifying the locker
         :return: depends on process response/execution
         """
+        # Start logging of Process
+        self.logging_cls = logging.ServiceLogging(
+                                    self.actor,
+                                    token,
+                                    self.session,
+                                    payload=request.data or dict())
+        self.logging_cls.initialize()  # initialize process logging
+
         process_locker_obj = self.get_locker_object(token)
 
         response = self._read_process_locker_rules(process_locker_obj)
 
         return Response(response, status=status.HTTP_200_OK)
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        """Log Process before sending final response
+
+        :param request: django request object
+        :param response: response to be sent to client
+        :param args: function arguments
+        :param kwargs: function keyword arguments
+        :return: returns final response
+
+        Note :
+            if response code is 2xx then we call success log method else false method will be called
+        """
+        if str(response.status_code).startswith('2'):
+            self.logging_cls.success(response)  # logged as success
+        else:
+            self.logging_cls.fail(response)  # logged as failure
+        return super(ProcessLockerViewSet, self).finalize_response(request, response, *args, **kwargs)
