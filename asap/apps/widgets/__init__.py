f72fed3e55d4a6b122c2a6d83d39de53f2ca66cf
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-01-20 12:06:41 +0530

    add : Widget service API :hammer: Widget API , binding with Process locker & logging

--- asap/apps/widgets/__init__.py
+++ asap/apps/widgets/__init__.py
@@ -3,3 +3,5 @@
           Process can be called based on some rules.Widget consists of a predefined Workflow.
 
 """
+
+PROCESS_LOCKER_UPSTREAM_URL = 'http://local.veris.in:8000/api/v1/process-locker/{process_locker_token}/'
--- asap/apps/widgets/views/widget.py
+++ asap/apps/widgets/views/widget.py
@@ -12,16 +12,23 @@
 # future
 from __future__ import unicode_literals
 
+# 3rd party
+import requests, uuid
+
 # DRF
-from rest_framework import response, status, viewsets, permissions
+from rest_framework import status, viewsets, permissions
+from rest_framework.response import Response
 from rest_framework.decorators import detail_route
+from rest_framework.exceptions import ValidationError
 
 # local
+from asap.apps.logs import logging
 from asap.core.views import AuthorableModelViewSet, DRFNestedViewMixin
 
 # own app
 from asap.apps.widgets.models.widget import Widget
 from asap.apps.widgets.serializers.widget import WidgetSerializer
+from asap.apps.widgets import PROCESS_LOCKER_UPSTREAM_URL
 
 
 class WidgetViewSet(AuthorableModelViewSet, DRFNestedViewMixin, viewsets.ModelViewSet):
@@ -33,6 +40,9 @@ class WidgetViewSet(AuthorableModelViewSet, DRFNestedViewMixin, viewsets.ModelVi
     permission_classes = (permissions.AllowAny, )
 
     lookup_field = 'token'
+    actor = 'widget'
+    session = uuid.uuid4()
+    logging_cls = None
 
     def make_queryset(self):
         """
@@ -48,13 +58,73 @@ class WidgetViewSet(AuthorableModelViewSet, DRFNestedViewMixin, viewsets.ModelVi
         # available to the requesting user for direct access
         return queryset
 
+    def _create_log_instance(self, request, token):
+        """
+        :param request : Django request object.
+        :param token : widget token, helps in identifying the widget
+        :return: logging class instance
+        """
+        self.logging_cls = logging.ServiceLogging(
+                                    self.actor,
+                                    token,
+                                    self.session,
+                                    payload=request.data or dict())
+
     @detail_route(methods=['post'], )
     def resolve_widget(self, request, token):
+        """Widget POST request handles by this method
+
+        :param request : Django request object.
+        :param token: widget token, helps in identifying the widget
+        :return: depends on widget response/execution
+        """
+        # Start logging of Widget
+        self._create_log_instance(request, token)
+        self.logging_cls.initialize()  # initialize widget logging
+
+        response = self._execute_process_locker(request.data or dict())
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    def _execute_process_locker(self, data):
         """
 
-        :param request: Django request object.
-        :param token: Widget token
         :return:
         """
+        process_locker_token = self.get_object().process_locker_token
 
-        return response.Response(status=status.HTTP_200_OK)
+        url = str(PROCESS_LOCKER_UPSTREAM_URL).format(
+            process_locker_token=process_locker_token
+        )
+        self.logging_cls.handshake(process_locker_token, data)  # execution handover initiated
+
+        rq = requests.post(url=url,
+                           data=data
+                           )
+        if rq.status_code == requests.codes.ok:
+            self.logging_cls.handshake_succeed(process_locker_token, data, rq)  # execution handover status
+            return rq.json()
+
+        self.logging_cls.handshake_failed(process_locker_token, data, rq.status_code, rq.json())  # execution handover status
+        raise ValidationError(rq.json())
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        """Log Process before sending final response
+
+        :param request: django request object
+        :param response: response to be sent to client
+        :param args: function arguments
+        :param kwargs: function keyword arguments
+        :return: returns final response
+
+        Note :
+            if response code is 2xx then we call success log method else false method will be called
+        """
+        if self.logging_cls is None:
+            self._create_log_instance(request, kwargs.get('token'))
+
+        if str(response.status_code).startswith('2'):
+            self.logging_cls.success(response)  # logged as success
+        else:
+            self.logging_cls.fail(response)  # logged as failure
+        return super(WidgetViewSet, self).finalize_response(request, response, *args, **kwargs)
