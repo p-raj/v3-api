22af19dc90b7007598f4c640cd65196562a8d3ad
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-01-30 15:54:29 +0530

    refactor : Improve Store app files structure :art:
    Updated app structure so that every app have uniform File/folder struture.

---
+++ asap/apps/store/core/__init__.py
@@ -0,0 +1,3 @@
+"""
+This file includes the main files/class/functions of core modules of store app like resource etc.
+"""
---
+++ asap/apps/store/views/__init__.py
@@ -0,0 +1,3 @@
+"""
+This pkg includes all views of store app, all HTTP interactions will points here.
+"""
