e0448482c329d0f3196fe8aff05b6590744e6dfa
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-01-31 19:05:39 +0530

    update : Validation added @ model level :construction_worker:

--- asap/apps/widgets/models/widget.py
+++ asap/apps/widgets/models/widget.py
@@ -13,7 +13,7 @@
 from __future__ import unicode_literals
 
 # 3rd party
-import uuid
+import uuid, bleach
 
 # Django
 from django.conf import settings
@@ -21,9 +21,11 @@ from django.contrib import admin
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.postgres.fields import JSONField
+from django.core.exceptions import ValidationError
 
 # local
 from asap.core.models import Authorable, Timestampable
+from asap.apps.utils import validator
 
 User = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
@@ -58,6 +60,27 @@ class Widget(Authorable, Timestampable, models.Model):
     # Functions
     def __str__(self):
         return '{0}'.format(self.name)
+
+    def clean(self):
+        """Validate models field data or clean fields data so that no bad strings can cause any problem.
+        """
+
+        # reject any malicious input string
+        bad_strings_json = validator._get_bad_strings_json().get('rejected_list')
+
+        if self.name in bad_strings_json:
+            raise ValidationError(_('malicious input string sent in name. {0}'.format(self.name)))
+
+        # validate char fields data length
+        if len(self.name) > 30:
+            raise ValidationError({'name': _('Length of name cannot be greater then 30')})
+
+        # clean or bleach fields data
+        self.name= bleach.clean(self.name)
+
+    def save(self, **kwargs):
+        self.clean()
+        return super(Widget, self).save(**kwargs)
 
 
 @admin.register(Widget)
