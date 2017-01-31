7666d4a49ab7eaad30a0802e72e6a00206477560
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-01-31 19:03:18 +0530

    test : Widget test cases added :white_check_mark:

---
+++ asap/apps/widgets/tests/base.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+# future
+from __future__ import unicode_literals
+
+# 3rd party
+import ujson as json
+import uuid
+# Django
+from django.test import TransactionTestCase
+from django.conf import settings
+from django.contrib.auth import get_user_model
+
+# own app
+from asap.apps.widgets import models
+
+User = get_user_model()
+
+
+class WidgetTestCase(TransactionTestCase):
+    """Base class of Widget Test Case, it includes all possible test cases on widgets.
+       Inherit this file and override respective test case method.
+
+    """
+    model = models.widget.Widget
+    locker = models.widget_locker.WidgetLocker
+    bad_strings_clean_list = None
+    bad_strings_reject_list = None
+    widget_obj = None
+    user = None
+
+    def setUp(self):
+        """
+
+        """
+        self.user = User.objects.create(username="lion")
+        self.widget_obj = self.model.objects.create(name="lion", process_locker_token=uuid.uuid4(), schema=dict(), author=self.user)
+
+        self.bad_strings_clean_list = self._get_bad_strings_json().get('cleaned_list')
+        self.bad_strings_reject_list = self._get_bad_strings_json().get('rejected_list')
+
+    def _get_bad_strings_json(self):
+        """
+
+        :return: bad string json object.
+        """
+        json_data = open('static/bad_strings.json').read()
+        return json.loads(json_data)
+
+    def upstream_url(self):
+        """
+        """
+        return getattr(settings, 'WIDGETS_MICRO_SERVICE', 'http://localhost:8000')
---
+++ asap/apps/widgets/tests/test_api.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+# future
+from __future__ import unicode_literals
+
+# 3rd party
+import uuid
+
+# Django
+from django.urls import reverse
+
+# DRF
+from rest_framework.test import RequestsClient
+
+# own app
+from asap.apps.widgets.tests import base
+
+client = RequestsClient()
+
+
+class WidgetsAPITestCase(base.WidgetTestCase):
+    """
+
+    """
+
+    def test_widget_objcet_api(self):
+        """
+        :return:
+        """
+        # test case for widget-detail (single object)
+
+        router = reverse('widget-detail', args=[str(self.widget_obj.token), ])
+        url = '{0}{1}'.format(self.upstream_url(), router)
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        # test case for widget-list (multiple objects)
+
+        router = reverse('widget-list')
+        url = '{0}{1}'.format(self.upstream_url(), router)
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_widget_locker_objcet_api(self):
+        """
+        :return:
+        """
+        locker = self.locker.objects.create(name='lion', token=uuid.uuid4(), rules=dict(), author=self.user)
+
+        # test case for widgetlocker-detail (single object)
+
+        router = reverse('widgetlocker-detail', args=[str(locker.token), ])
+        url = '{0}{1}'.format(self.upstream_url(), router)
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        # test case for widgetlocker-list (multiple objects)
+
+        router = reverse('widgetlocker-list')
+        url = '{0}{1}'.format(self.upstream_url(), router)
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
