bf72aa8e67a715c26c0f8475f9ee5933fd45ebc2
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-03-15 16:07:48 +0530

    Add : states micro service :hammer:

---
+++ asap/micro_services/state_machine/states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+- **file_path**
+~~~~~~~~~~~~~~
+
+- **file description**
+"""
+
+# future
+from __future__ import unicode_literals
+
+# 3rd party
+
+
+# Django
+
+
+# local
+
+
+# own app
