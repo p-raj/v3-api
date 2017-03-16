cc88f7bb51276ed3918d350012388359f59e27ee
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-03-16 12:38:06 +0530

    Add : state_machine micro service router and Viewsets :hammer:

--- asap/micro_services/state_machine/router.py
+++ asap/micro_services/state_machine/router.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-- **file_path**
+- micro_services.state_machine.router
 ~~~~~~~~~~~~~~
 
-- **file description**
+- Routers of state_machine micro-service
 """
 
 # future
@@ -15,9 +15,42 @@ from __future__ import unicode_literals
 
 
 # Django
+from django.conf.urls import url
 
 
 # local
 
 
 # own app
+from asap.micro_services.state_machine import views
+
+create_initial_state = views.TransactionStateViewSet.as_view({
+    'post': 'create_initial_state',
+})
+
+get_current_state = views.TransactionStateViewSet.as_view({
+    'get': 'get_current_state',
+})
+
+get_complete_transaction_life_cycle = views.TransactionStateViewSet.as_view({
+    'get': 'get_complete_transaction_life_cycle',
+})
+
+change_state = views.TransactionStateViewSet.as_view({
+    'put': 'change_state',
+})
+
+urlpatterns = [
+    url(r'^(?P<task_identifier>[0-9a-z-]+)/new/$',
+        create_initial_state,
+        name='create-initial-state'),
+    url(r'^(?P<task_identifier>[0-9a-z-]+)/current/$',
+        get_current_state,
+        name='get-current-state'),
+    url(r'^(?P<task_identifier>[0-9a-z-]+)/life_cycle/$',
+        get_complete_transaction_life_cycle,
+        name='get-complete-transaction-life-cycle'),
+    url(r'^(?P<task_identifier>[0-9a-z-]+)/change/$',
+        change_state,
+        name='change-state'),
+]
--- asap/micro_services/state_machine/views.py
+++ asap/micro_services/state_machine/views.py
@@ -2,17 +2,18 @@
 # -*- coding: utf-8 -*-
 
 """
-- **file_path**
+- micro_services.state_machine.views
 ~~~~~~~~~~~~~~
 
-- **file description**
+- This file contains state_machine micro-service views, every HTTP request/router points to this file.
 """
 
 # future
 from __future__ import unicode_literals
 
 # 3rd party
-
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
 
 # Django
 
@@ -21,3 +22,41 @@ from __future__ import unicode_literals
 
 
 # own app
+from asap.micro_services.state_machine.models import TransactionLifeCycle, TransactionStateMachine, HttpService
+
+
+class TransactionStateViewSet(viewsets.GenericViewSet):
+    """States viewset , it controls general functions of states like create new, change and get current state
+
+    """
+    model = TransactionStateMachine
+    # TODO : remove AllowAny permission with proper permission class
+    permission_classes = (permissions.AllowAny, )
+
+    def create_initial_state(self, task_identifier):
+        """
+
+        :return:
+        """
+        pass
+
+    def get_current_state(self, task_identifier):
+        """
+
+        :return:
+        """
+        pass
+
+    def get_complete_transaction_life_cycle(self, task_identifier):
+        """
+
+        :return:
+        """
+        pass
+
+    def change_state(self, task_identifier):
+        """
+
+        :return:
+        """
+        pass
