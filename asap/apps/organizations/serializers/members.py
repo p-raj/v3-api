3668f77df6c878839d4a0fd8b2a67b336c4ba1e5
Ankit Popli <ankit.popli@veris.in>
2017-03-21 16:43:55 +0530

    fix: user object is required, awaiting long due fix :bug:

--- asap/apps/organizations/serializers/members.py
+++ asap/apps/organizations/serializers/members.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from asap.apps.authentication.views.users import UserSerializer
 from asap.apps.organizations.serializers.organizations import OrganizationSerializer
 from asap.fields.hyperlinked_serialized_related_field import HyperlinkedSerializedRelatedField
 from ..models.members import Member
@@ -7,6 +8,11 @@ from ..models.members import Member
 
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.ReadOnlyField()
+    user = HyperlinkedSerializedRelatedField(
+        read_only=True,
+        view_name='user-detail',
+        serializer=UserSerializer
+    )
     organization = HyperlinkedSerializedRelatedField(
         read_only=True,
         view_name='organization-detail',
