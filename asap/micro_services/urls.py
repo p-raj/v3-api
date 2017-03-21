d679c37f5440f2239ac70a9761361c0dbc270815
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-03-21 16:41:37 +0530

    Add : authorization micro-service routers :hammer:

--- asap/micro_services/urls.py
+++ asap/micro_services/urls.py
@@ -10,8 +10,10 @@ from django.conf.urls import url, include
 # local apps
 from asap.micro_services.notification import router as notification_router
 from asap.micro_services.state_machine import router as states_router
+from asap.micro_services.authorization import router as authorize_router
 
 urlpatterns = [
     url(r'^notification/', include(notification_router)),
     url(r'^state/', include(states_router)),
+    url(r'^authorize/', include(authorize_router)),
 ]
