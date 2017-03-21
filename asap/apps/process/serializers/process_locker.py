a17112a64965f3c901f5d92f5fd22bb3e1b99186
Ankit Popli <ankit.popli@veris.in>
2017-03-21 16:39:00 +0530

    feat: allow adding processes to existing organization
        - alter the existing lockers

--- asap/apps/process/serializers/process_locker.py
+++ asap/apps/process/serializers/process_locker.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from asap.apps.process.models import ProcessLocker
-from asap.core.serializers import TimestampableModelSerializer, AuthorableModelSerializer
-
 from rest_framework import serializers
+
+from asap.apps.process.models import ProcessLocker
+from asap.apps.process.models.process import Process
+from asap.apps.process.serializers.process import ProcessSerializer
+from asap.core.serializers import AuthorableModelSerializer, TimestampableModelSerializer
+from asap.fields.hyperlinked_serialized_related_field import HyperlinkedSerializedRelatedField
 
 
 class ProcessLockerSerializer(AuthorableModelSerializer, TimestampableModelSerializer,
                               serializers.HyperlinkedModelSerializer):
     token = serializers.CharField(read_only=True)
+    processes = HyperlinkedSerializedRelatedField(
+        view_name='process-detail', many=True,
+        serializer=ProcessSerializer,
+        queryset=Process.objects.all(),
+        style={
+            'base_template': 'input.html'
+        },
+        lookup_field='uuid'
+    )
 
     class Meta:
         model = ProcessLocker
--- asap/apps/process/views/process_locker.py
+++ asap/apps/process/views/process_locker.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+
 from asap.apps.process.models import ProcessLocker
+from asap.apps.process.serializers import ProcessSerializer
 from asap.apps.process.serializers.process_locker import ProcessLockerSerializer
 from asap.core.filters.author_filter import AuthorFilter
 from asap.core.permissions.is_author_or_read_only import IsAuthorOrReadOnly
 from asap.core.views import AuthorableModelViewSet
-
-from rest_framework import viewsets
 
 
 class ProcessLockerViewSet(AuthorableModelViewSet, viewsets.ModelViewSet):
@@ -17,3 +19,17 @@ class ProcessLockerViewSet(AuthorableModelViewSet, viewsets.ModelViewSet):
     filter_backends = (AuthorFilter,)
 
     lookup_field = 'uuid'
+
+    @detail_route(methods=['POST'], lookup_field='uuid')
+    def add(self, request, **kwargs):
+        # expect either an existing instance
+        # object/url or a new instance
+        # FIXME
+        # not perfect yet, but works :/
+        process_serializer = ProcessSerializer(data=request.data, context=self.get_serializer_context())
+        process_serializer.is_valid(raise_exception=True)
+        process = process_serializer.save(author=request.user)
+
+        process_locker = self.get_object()
+        process_locker.processes.add(process)
+        return self.retrieve(request, None, **kwargs)
