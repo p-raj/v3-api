eb8b3d3d30100038a91cc25ba57887097582df4e
Ankit Popli <ankit.popli@veris.in>
2017-07-21 20:18:30 +0530

    refactor: make the code more readable, & manageable
     - start logical segregating data as context, env & config

--- asap/apps/runtime/views/widget_process_execution.py
+++ asap/apps/runtime/views/widget_process_execution.py
@@ -3,6 +3,7 @@ import logging
 from time import sleep
 
 import requests
+from box import Box
 from mistralclient.api.v2.executions import ExecutionManager
 from rest_framework import response, views
 from rest_framework.parsers import FormParser, JSONParser, MultiPartParser
@@ -26,49 +27,9 @@ class WidgetProcessExecution(views.APIView):
         JSONParser, MultiPartParser
     )
 
-    def get_session(self):
-        return self.request.META.get('HTTP_X_VRT_SESSION', '')
-
-    def get_process_url(self, request, **kwargs):
-        return reverse_lazy(
-            'process-execute',
-            request=request,
-            kwargs={
-                'uuid': kwargs.get('action')
-            }
-        )
-
     def post(self, request, *args, **kwargs):
         logger.debug('content-type: %s', request.content_type)
-
-        config = Config.objects.filter(
-            widget__uuid=kwargs.get('widget_uuid'),
-            process__uuid=kwargs.get('action')
-        ).first()
-        data = config.config or {} if config else {}
-        logger.debug('widget config: %s', data)
-
-        username = ''
-        if self.get_session():
-            session = Session.objects.filter(uuid=self.get_session()).first()
-            if session:
-                username = session.author.username
-            else:
-                logger.debug('invalid session: %s', session)
-
-        # FIXME
-        # use AST instead of this hack
-        body = json.loads(
-            json.dumps(data)
-                .replace('$.auth', username)
-                .replace('$.session', self.get_session())
-                .replace('$.widget', kwargs.get('widget_uuid'))
-                .replace('$.process', kwargs.get('action'))
-        )
-        body.update(**request.data)
-
-        if config and config.transform:
-            body = transform(body, config.transform.get('@request', {}))
+        body = self.get_action_data(request, *args, **kwargs)
 
         if body.get('workflow_name', None):
             em = ExecutionManager(MistralHTTPClient())
@@ -95,9 +56,8 @@ class WidgetProcessExecution(views.APIView):
             )
 
         resp = requests.post(
-            self.get_process_url(request, **kwargs),
-            json=body,
-            params=dict(request.query_params)
+            self.get_process_url(request, kwargs.get('action')),
+            json=body, params=dict(request.query_params)
         )
 
         data = resp.json()
@@ -107,4 +67,99 @@ class WidgetProcessExecution(views.APIView):
             status=resp.status_code,
             template_name=None,
             headers=resp.headers
+        )
+
+    def get_action_data(self, request, *args, **kwargs):
+        if not request.content_type.startswith('application/json'):
+            return request.data
+
+        # FIXME
+        # use AST instead of this hack
+        # replace variables, for backward compatibility
+        # YAQL should take care of it in future
+        data = json.loads(
+            json.dumps(self.data)
+                .replace('$.auth', self.user.username)
+                .replace('$.session', self.session)
+                .replace('$.widget', self.environment.widget)
+                .replace('$.process', self.environment.process)
+        )
+
+        if not self.expressions:
+            # for backwards compatibility
+            d = data.get('@config')
+            d.update(**data.get('@context'))
+            return d
+
+        return transform(data, self.expressions)
+
+    # noinspection PyAttributeOutsideInit
+    def initial(self, request, *args, **kwargs):
+        super(WidgetProcessExecution, self).initial(request, *args, **kwargs)
+
+        # Runs anything that needs to occur prior to calling
+        # the method handler :)
+
+        # session is a pre-requisite
+        self.session = self.request.META.get('HTTP_X_VRT_SESSION', '')
+        # there are a lot of places where session is not yet compulsory :/
+        # so let's just log it
+        logger.debug('session: %s', self.session)
+        # assert not self.session, 'session not provided, ' \
+        #                          'consider sending a valid X-VRT-SESSION header'
+
+        # the viewset is unprotected to be used as a workflow callback :/
+        # so for now we need to have the user determined by the session
+        # instead of the request.
+        # FIXME
+        self.user = request.user
+        if self.session:
+            session = Session.objects.filter(uuid=self.session).first()
+            self.user = session.author
+            # assert not self.user.is_authenticated(), 'user must authenticated'
+            logger.debug('req user: %s', request.user)
+            logger.debug('session user: %s', self.user)
+
+        # the data available can be divided in 3 parts
+        # context       - data from the client: eg. user input, & user information
+        #               the context will only be set if the content-type is application/json
+        # environment   - info like widget/process/session
+        # configuration - data saved in the widget config,
+        #               used for storing sensitive info like secrets
+        self.context = request.data \
+            if request.content_type.startswith('application/json') \
+            else {}
+
+        widget = kwargs.get('widget_uuid')
+        process = kwargs.get('action')
+        self.environment = Box({
+            'widget': widget,
+            'process': process,
+            'session': self.session
+        })
+
+        config = Config.objects.filter(
+            widget__uuid=widget,
+            process__uuid=process
+        ).first()
+        self.config = config.config or {} if config else {}
+
+        self.data = {
+            '@context': self.context,
+            '@config': self.config,
+            '@env': self.environment,
+        }
+
+        # TODO
+        # separate expression for req/resp
+        self.expressions = config.transform or {}
+
+    @staticmethod
+    def get_process_url(request, process_id):
+        return reverse_lazy(
+            'process-execute',
+            request=request,
+            kwargs={
+                'uuid': process_id
+            }
         )
